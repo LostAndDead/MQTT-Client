d9a975c9b64f6de9bc7184781de12c063ca7e194
LostAndDead <jakemarsh9012@gmail.com>
2021-01-19 16:02:47 +0000

    added tls support

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 var mqtt = require('mqtt');
+var fs = require('fs')
 
 const readline = require('readline');
 const sl = require("serverline")
 
 var ip;
+var p;
+var keyFile;
+var keyFilePath;
+var certFile;
+var certFilePath;
+var caFile;
+var caFilePath;
 var subChannel;
 var pubChannel;
 var log = true;
 
 var ipDefault = "192.168.1.109"
+var portDefault = "8883"
+var keyFileDefault = "./certs/user1.key"
+var certFileDefault = "./certs/user1.crt"
+var caFileDefault = "./certs/ca.crt"
 var subChannelDefault = "test"
 var pubChannelDefault = "test"
 
@@ -19,6 +31,48 @@ async function main(){
     ip = await askQuestion(`Enter the IP or FQDN of the Mosquitto Server \x1b[4m(${ipDefault})\n\x1b[0m> `)
     if (ip == ""){
         ip = ipDefault
+    }
+
+    console.clear()
+    p = await askQuestion(`Enter the port for the Mosquitto Server \x1b[4m(${portDefault})\n\x1b[0m> `)
+    if (p == ""){
+        p = portDefault
+    }
+
+    console.clear()
+    keyFilePath = await askQuestion(`Enter the file path to your key file. \x1b[4m(${keyFileDefault})\n\x1b[0m> `)
+    if (keyFilePath == ""){
+        keyFilePath = keyFileDefault
+    }
+    try {
+        keyFile = fs.readFileSync(keyFilePath, 'utf8')
+        console.log(data)
+    } catch (err) {
+        console.error(err)
+    }
+
+    console.clear()
+    certFilePath = await askQuestion(`Enter the file path to your certificate file. \x1b[4m(${certFileDefault})\n\x1b[0m> `)
+    if (certFilePath == ""){
+        certFilePath = certFileDefault
+    }
+    try {
+        certFile = fs.readFileSync(certFilePath, 'utf8')
+        console.log(data)
+    } catch (err) {
+        console.error(err)
+    }
+
+    console.clear()
+    caFilePath = await askQuestion(`Enter the file path to your certificate authority file. \x1b[4m(${caFileDefault})\n\x1b[0m> `)
+    if (caFilePath == ""){
+        caFilePath = caFileDefault
+    }
+    try {
+        caFile = fs.readFileSync(caFilePath, 'utf8')
+        console.log(data)
+    } catch (err) {
+        console.error(err)
     }
 
     console.clear()
@@ -34,13 +88,29 @@ async function main(){
     }
 
     console.clear()
-    console.log(`\x1b[34mSubscribed to ${subChannel}\n\x1b[33mPublishing to ${pubChannel}\n\x1b[0mType message to send or close to \x1b[31mclose\n\x1b[33m`)
+    console.log(`\x1b[0mConnected to \x1b[35m${ip + "\x1b[37m:\x1b[32m" + p}`)
+    console.log(`\x1b[0m\nKey File: \x1b[4m${caFilePath}`)
+    console.log(`\x1b[0mCertificate File: \x1b[4m${caFilePath}`)
+    console.log(`\x1b[0mCertificate Authority File: \x1b[4m${caFilePath}`)
+    console.log(`\n\x1b[0m\x1b[34mSubscribed to ${subChannel}`)
+    console.log(`\x1b[33mPublishing to ${pubChannel}`)
+    console.log(`\n\x1b[0mType message to send or close to \x1b[31mclose\n\x1b[33m`)
     connect();
 }
 
 function connect(){
 
-    var client = mqtt.connect(`mqtt://${ip}`)
+    var options = {
+        port: p,
+        host: ip,
+        key: keyFile,
+        cert: certFile,
+        rejectUnauthorized: true,
+        ca: caFile,
+        protocol: 'mqtts'
+    }
+
+    var client = mqtt.connect(options)
 
     client.on('connect', function () {
         client.subscribe(subChannel, function (err) {
